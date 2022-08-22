0856bf7d3fcd4796f6b3c295f4725f06a1d67a56
Aminulhoque1 <aminulkupa50@gmail.com>
2022-08-22 11:41:16 -0700

    done

--- js/app.js
+++ js/app.js
@@ -1,20 +1,22 @@
 
 const array=[];
 
-function getBtnId(element){
-    const name= element.parentNode.children[0].innerText;
+    function getBtnId(element){
+        const name= element.parentNode.children[0].innerText;
+        
+        const object={
+            name: name,
+        }
+        array.push(object);
+        
+
+        display(array)
+
+        
     
-    const object={
-        name: name,
     }
-    array.push(object);
-    
 
-    display(array)
-
- 
-}
-
+   
     function getElementById(elementId){
         const saveBtn= document.getElementById(elementId);
         const btn= saveBtn.disabled= true;
@@ -22,7 +24,7 @@ function getBtnId(element){
     }
 
 
-
+   
     const saveBtn=document.querySelector('#save-btn');
     saveBtn.addEventListener('click', function()  {
         saveBtn.style.backgroundColor='#dddddd' ;
@@ -77,7 +79,7 @@ function getBtnId(element){
         getElementById("save-btn9")
     });
 
-   
+  
     
 
 
@@ -85,73 +87,74 @@ function getBtnId(element){
     
 
 
-function display(name){
-    
-    
-    
-    if(name.length  > 5){
-        alert('you choices 5 players');
-        return;
-    }else{
-        const cartContainer = document.getElementById("list-parent");
-        cartContainer.textContent = '';
-        for(let i = 0; i < name.length; i++){
-            const tr = document.createElement("tr");
-            tr.innerHTML = `
-            <th>${i+1}</th>
-            <td>${name[i].name}</td>
-            
-            `;
+    function display(name){
+        
+        
+        
+        if(name.length  > 5){
+            alert('you choices 5 players');
+            return;
+        }else{
+            const cartContainer = document.getElementById("list-parent");
+            cartContainer.textContent = '';
+            for(let i = 0; i < name.length; i++){
+                const tr = document.createElement("tr");
+                tr.innerHTML = `
+                <th>${i+1}</th>
+                <td>${name[i].name}</td>
+                
+                `;
+                cartContainer.appendChild(tr);
+                
+
+            }
+
+            const tr = document.createElement('tr');
             cartContainer.appendChild(tr);
+        
+           
+        }
+        const totalPlayer= name.length;
+            
+    
+
+        document.getElementById('Calculate-btn').addEventListener('click', function(){
+        
+            const Calculate= getInputValueId('money input');
+            
+            
+            const calculetIntu= Calculate * totalPlayer;
+        
+            const totalExpance= document.getElementById('player-cost');
+            totalExpance.innerText= calculetIntu;
             
 
-        }
+            
+        })
 
-        const tr = document.createElement('tr');
-        cartContainer.appendChild(tr);
-    
+
+        document.getElementById('Calculate-total').addEventListener('click', function(){
+            const Calculate= getInputValueId('money input');
+            const calculetIntu= Calculate * totalPlayer;
+            const managerFill= getInputValueId('manegar-input');
+            const coachFill= getInputValueId('coach-input');
+        
+            const toatalCost= calculetIntu + managerFill + coachFill;
+        
+            const costFill= document.getElementById('total-cost');
+            costFill.innerText =toatalCost;
+        })
     }
-    const totalPlayer= name.length;
-        
-   
-
-    document.getElementById('Calculate-btn').addEventListener('click', function(){
-    
-        const Calculate= getInputValueId('money input');
-        
-        
-        const calculetIntu= Calculate * totalPlayer;
-    
-        const totalExpance= document.getElementById('player-cost');
-        totalExpance.innerText= calculetIntu;
-        
-
-        
-    })
-
-
-    document.getElementById('Calculate-total').addEventListener('click', function(){
-        const Calculate= getInputValueId('money input');
-        const calculetIntu= Calculate * totalPlayer;
-        const managerFill= getInputValueId('manegar-input');
-        const coachFill= getInputValueId('coach-input');
-    
-        const toatalCost= calculetIntu + managerFill + coachFill;
-    
-        const costFill= document.getElementById('total-cost');
-        costFill.innerText =toatalCost;
-    })
-}
 
 
 
 
-function getInputValueId(input){
-    const CalculateFill= document.getElementById(input);
-    const CalculateString= CalculateFill.value;
-    const CalculateNumber= parseFloat(CalculateString);
-    return CalculateNumber;
-}
+    function getInputValueId(input){
+        const CalculateFill= document.getElementById(input);
+        const CalculateString= CalculateFill.value;
+        const CalculateNumber= parseFloat(CalculateString);
+        return CalculateNumber;
+    }
 
 
 
